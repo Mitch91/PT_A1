9252ab55f04560b56dc2e4b511b57ec8d43e1620
Mitchell Elsbury <gl1tch@live.com.au>
2013-08-05 00:04:06 +1000

    add Perfect Sqaures functionality and return to menu

--- assign1.c
+++ assign1.c
@@ -13,7 +13,49 @@
 int main(void)
 {
     int option_stats[NUM_OPTION_STATS];
-    /* loop for displaying the main menu and running the selected
-     * option here */
-    return EXIT_SUCCESS;
+	unsigned number;
+	OPTION option_number;
+	while(TRUE){
+		dispOptions();
+		printf("Select your option: ");
+		
+		/* get an integer from the user from the range 
+		   of the first option to the last */
+		option_number = getInt(PERFECT_SQUARES,EXIT);
+		
+		switch(option_number){
+			case PERFECT_SQUARES: 
+				printf("Enter a positive integer (%d - %d): ", 
+					PERFECT_SQUARES_MIN, PERFECT_SQUARES_MAX);
+					
+				/* get a number within in the range from the user */
+				number = getInt(PERFECT_SQUARES_MIN, PERFECT_SQUARES_MAX);
+				if(number == RETURN_TO_MENU) break;
+				/* conduct our analysis */
+				perfect_squares(option_stats, number);
+				break;
+			
+			case ASCII_TO_BINARY:
+				printf("Ascii\n");
+				break;
+				
+			case MATCHING_BRACKETS:
+				printf("brackets\n");
+				break;
+			
+			case FORMATTED_TEXT:
+				printf("formatted text\n");
+				break;
+				
+			case SESSION_SUMMARY:
+				printf("summary\n");
+				break;
+				
+			case EXIT:
+				return EXIT_SUCCESS;
+				
+			default:
+				printf("invalid\n");
+		}
+	}
 }
